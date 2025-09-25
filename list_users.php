aa73d9f1aa250466df2006594a3fb616ae80c3be
MinhTuan911 <ledongminhtuan@gmail.com>
2025-09-25 10:46:18 +0700

    update xxs, sql injection security

--- list_users.php
+++ list_users.php
@@ -41,24 +41,24 @@ $users = $userModel->getUsers($params);
                 <tbody>
                     <?php foreach ($users as $user) { ?>
                         <tr>
-                            <th scope="row"><?php echo $user['id'] ?></th>
+                            <th scope="row"><?php echo htmlspecialchars($user['id'], ENT_QUOTES, 'UTF-8'); ?></th>
                             <td>
-                                <?php echo $user['name'] ?>
+                            <td><?php echo htmlspecialchars($user['name'], ENT_QUOTES, 'UTF-8'); ?></td>
                             </td>
                             <td>
-                                <?php echo $user['fullname'] ?>
+                            <td><?php echo htmlspecialchars($user['fullname'], ENT_QUOTES, 'UTF-8'); ?></td>
                             </td>
                             <td>
-                                <?php echo $user['type'] ?>
+                            <td><?php echo htmlspecialchars($user['type'], ENT_QUOTES, 'UTF-8'); ?></td>
                             </td>
                             <td>
-                                <a href="form_user.php?id=<?php echo $user['id'] ?>">
+                             <a href="form_user.php?id=<?php echo urlencode($user['id']); ?>">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true" title="Update"></i>
                                 </a>
-                                <a href="view_user.php?id=<?php echo $user['id'] ?>">
+                              <a href="view_user.php?id=<?php echo urlencode($user['id']); ?>">
                                     <i class="fa fa-eye" aria-hidden="true" title="View"></i>
                                 </a>
-                                <a href="#" class="btn-delete" data-id="<?php echo $user['id'] ?>">
+                                 <a href="#" class="btn-delete" data-id="<?php echo htmlspecialchars($user['id'], ENT_QUOTES, 'UTF-8'); ?>">
                                     <i class="fa fa-eraser" aria-hidden="true" title="Delete"></i>
                                 </a>
                             </td>
--- models/UserModel.php
+++ models/UserModel.php
@@ -2,17 +2,24 @@
 
 require_once 'BaseModel.php';
 
-class UserModel extends BaseModel {
+class UserModel extends BaseModel
+{
 
-    public function findUserById($id) {
-        $sql = 'SELECT * FROM users WHERE id = '.$id;
-        $user = $this->select($sql);
-
+    public function findUserById($id)
+    {
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $stmt = self::$_connection->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
         return $user;
     }
 
-    public function findUser($keyword) {
-        $sql = 'SELECT * FROM users WHERE user_name LIKE %'.$keyword.'%'. ' OR user_email LIKE %'.$keyword.'%';
+    public function findUser($keyword)
+    {
+        $sql = 'SELECT * FROM users WHERE user_name LIKE %' . $keyword . '%' . ' OR user_email LIKE %' . $keyword . '%';
         $user = $this->select($sql);
 
         return $user;
@@ -24,11 +31,16 @@ class UserModel extends BaseModel {
      * @param $password
      * @return array
      */
-    public function auth($userName, $password) {
+    public function auth($userName, $password)
+    {
         $md5Password = md5($password);
-        $sql = 'SELECT * FROM users WHERE name = "' . $userName . '" AND password = "'.$md5Password.'"';
-
-        $user = $this->select($sql);
+        $sql = "SELECT * FROM users WHERE name = ? AND password = ?";
+        $stmt = self::$_connection->prepare($sql);
+        $stmt->bind_param("ss", $userName, $md5Password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
         return $user;
     }
 
@@ -37,10 +49,14 @@ class UserModel extends BaseModel {
      * @param $id
      * @return mixed
      */
-    public function deleteUserById($id) {
-        $sql = 'DELETE FROM users WHERE id = '.$id;
-        return $this->delete($sql);
-
+    public function deleteUserById($id)
+    {
+        $sql = "DELETE FROM users WHERE id = ?";
+        $stmt = self::$_connection->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $stmt->close();
+        return true;
     }
 
     /**
@@ -48,10 +64,11 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-    public function updateUser($input) {
+    public function updateUser($input)
+    {
         $sql = 'UPDATE users SET 
-                 name = "' . mysqli_real_escape_string(self::$_connection, $input['name']) .'", 
-                 password="'. md5($input['password']) .'"
+                 name = "' . mysqli_real_escape_string(self::$_connection, $input['name']) . '", 
+                 password="' . md5($input['password']) . '"
                 WHERE id = ' . $input['id'];
 
         $user = $this->update($sql);
@@ -64,14 +81,15 @@ class UserModel extends BaseModel {
      * @param $input
      * @return mixed
      */
-   public function insertUser($input) {
-    $name = $input['name'] ?? '';
-    $fullname = $input['fullname'] ?? '';
-    $email = $input['email'] ?? '';
-    $type = $input['type'] ?? '';
-    $password = md5($input['password'] ?? '');
+    public function insertUser($input)
+    {
+        $name = $input['name'] ?? '';
+        $fullname = $input['fullname'] ?? '';
+        $email = $input['email'] ?? '';
+        $type = $input['type'] ?? '';
+        $password = md5($input['password'] ?? '');
 
-    $sql = "INSERT INTO `app_web1`.`users` 
+        $sql = "INSERT INTO `app_web1`.`users` 
         (`name`, `fullname`, `email`, `type`, `password`) VALUES (
             '" . mysqli_real_escape_string(self::$_connection, $name) . "', 
             '" . mysqli_real_escape_string(self::$_connection, $fullname) . "', 
@@ -80,31 +98,28 @@ class UserModel extends BaseModel {
             '" . $password . "'
         )";
 
-    return $this->insert($sql);
-}
+        return $this->insert($sql);
+    }
 
     /**
      * Search users
      * @param array $params
      * @return array
      */
-    public function getUsers($params = []) {
-        //Keyword
+    public function getUsers($params = [])
+    {
         if (!empty($params['keyword'])) {
-            $sql = 'SELECT * FROM users WHERE name LIKE "%' . $params['keyword'] .'%"';
-
-            //Keep this line to use Sql Injection
-            //Don't change
-            //Example keyword: abcef%";TRUNCATE banks;##
-            $users = self::$_connection->multi_query($sql);
-
-            //Get data
-            $users = $this->query($sql);
+            $sql = "SELECT * FROM users WHERE name LIKE CONCAT('%', ?, '%')";
+            $stmt = self::$_connection->prepare($sql);
+            $stmt->bind_param("s", $params['keyword']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $users = $result->fetch_all(MYSQLI_ASSOC);
+            $stmt->close();
         } else {
             $sql = 'SELECT * FROM users';
             $users = $this->select($sql);
         }
-
         return $users;
     }
 }
